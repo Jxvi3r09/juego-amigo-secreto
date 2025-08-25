d054879641e42dca0bc9585b63019896b2c06c6d
Oscar Gomez <oscarjaviergomez12@gmail.com>
2025-08-24 20:15:31 -0500

    Funcion para listar lo0s amigos secretos

--- app.js
+++ app.js
@@ -13,7 +13,26 @@ function agregarAmigo() {
     amigoSecreto.push(nombre);
     // limpiamos el campo de entrada, despues de agregar un nuevo amigo
     document.getElementById("amigo").value = "";
+    actualizarAmigos();
     console.log(amigoSecreto);
   }
 }
 
+// funcion para mostrar la lista de amigos
+function actualizarAmigos() {
+  // limpiamos el contenido actual de la lista
+  document.getElementById("listaAmigos").innerHTML = "";
+  // Si la lista no está vacía, crea un título y agrégalo
+  if (amigoSecreto.length > 0) {
+    let titulo = document.createElement("h3");
+    titulo.textContent = "Lista de Amigos";
+    document.getElementById("listaAmigos").appendChild(titulo);
+  }
+  // iteramos sobre la lista y creamos la lista
+  for (let i = 0; i < amigoSecreto.length; i++) {
+    // creamos un nuevo elemento en la lista
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(amigoSecreto[i]));
+    document.getElementById("listaAmigos").appendChild(li);
+  }
+}
